9f66aba16e5ccc2257d0972dfb82d4418c4289bc
moheranus <danielshobe90@gmail.com>
2022-04-14 23:01:30 +0300

    started

---
+++ client/src/components/SidebarData.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as RiIcons from 'react-icons/ri';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/overview',
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Research',
+        path: '/overview/users',
+        icon: <IoIcons.IoIosPaper />
+      },
+      {
+        title: 'Thesis',
+        path: '/overview/revenue',
+        icon: <IoIcons.IoIosPaper />
+      }
+    ]
+  },
+  {
+    title: 'Next Meeting',
+    path: '/reports',
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Meeting 1',
+        path: '/reports/reports1',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Meeting 2',
+        path: '/reports/reports2',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav'
+      },
+      // {
+      //   title: 'Reports 3',
+      //   path: '/reports/reports3',
+      //   icon: <IoIcons.IoIosPaper />
+      // }
+    ]
+  },
+  {
+    title: 'Send draft',
+    path: '/products',
+    icon: <FaIcons.FaCartPlus />
+  },
+  {
+    title: 'Group',
+    path: '/team',
+    icon: <IoIcons.IoMdPeople />
+  },
+  {
+    title: 'Pending Comments',
+    path: '/messages',
+    icon: <FaIcons.FaEnvelopeOpenText />,
+
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Message 1',
+        path: '/messages/message1',
+        icon: <IoIcons.IoIosPaper />
+      },
+      {
+        title: 'Message 2',
+        path: '/messages/message2',
+        icon: <IoIcons.IoIosPaper />
+      }
+    ]
+  },
+  {
+    title: 'Support',
+    path: '/support',
+    icon: <IoIcons.IoMdHelpCircle />
+  }
+];
---
+++ client/src/components/SubMenu.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SidebarLink = styled(Link)`
+  display: flex;
+  color: #000000;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  list-style: none;
+  height: 60px;
+  text-decoration: none;
+  font-size: 18px;
+
+  &:hover {
+    background: #FF82A8;
+    border-left: 5px solid #632ce4;
+    border-radius: 30px;
+    cursor: pointer;
+  }
+`;
+
+const SidebarLabel = styled.span`
+  margin-left: 16px;
+`;
+
+const DropdownLink = styled(Link)`
+  background: #f5f5f5;
+  height: 60px;
+  padding-left: 3rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #000000;
+  font-size: 18px;
+
+  &:hover {
+    color: #FFFF;
+    background: #0c3866;
+    cursor: pointer;
+  }
+`;
+
+const SubMenu = ({ item }) => {
+  const [subnav, setSubnav] = useState(false);
+
+  const showSubnav = () => setSubnav(!subnav);
+
+  return (
+    <>
+      <SidebarLink to={item.path} onClick={item.subNav && showSubnav}>
+        <div>
+          {item.icon}
+          <SidebarLabel>{item.title}</SidebarLabel>
+        </div>
+        <div>
+          {item.subNav && subnav
+            ? item.iconOpened
+            : item.subNav
+            ? item.iconClosed
+            : null}
+        </div>
+      </SidebarLink>
+      {subnav &&
+        item.subNav.map((item, index) => {
+          return (
+            <DropdownLink to={item.path} key={index}>
+              {item.icon}
+              <SidebarLabel>{item.title}</SidebarLabel>
+            </DropdownLink>
+          );
+        })}
+    </>
+  );
+};
+
+export default SubMenu;
