d45ec298f3e7c8c53237332dca88ef40ca363fc7
moheranus <danielshobe90@gmail.com>
2022-04-18 21:53:18 +0300

    notif

--- client/src/pages/Notifications.js
+++ client/src/pages/Notifications.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Notifications(){
     return (
         <>
-        <h1 >Notifications</h1>
+        <h1 >Notifications here</h1>
         </>
     );
 }
